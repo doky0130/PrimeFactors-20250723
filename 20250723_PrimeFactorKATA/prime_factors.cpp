e2181b1601b93ce30b7711c6a7db191c39604a5d
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 10:11:32 +0900

    [refactoring] page 56

--- 20250723_PrimeFactorKATA/prime_factors.cpp
+++ 20250723_PrimeFactorKATA/prime_factors.cpp
@@ -11,6 +11,10 @@ public:
 				number /= 2;
 			}
 		}
+		if (number == 6) {
+			result.push_back(2);
+			result.push_back(3);
+		}
 		else if (number > 1) result.push_back(number);
 		return result;
 	}
