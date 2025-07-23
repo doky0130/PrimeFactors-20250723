3cbf56d7c3db482ae78288cd023d1a2740375a4f
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 10:10:32 +0900

    [refactoring] page 54

--- 20250723_PrimeFactorKATA/prime_factors.cpp
+++ 20250723_PrimeFactorKATA/prime_factors.cpp
@@ -6,11 +6,7 @@ public:
 	vector<int> of(int number) {
 		vector<int> result;
 		if (number == 4) {
-			if (number % 2 == 0) {
-				result.push_back(2);
-				number /= 2;
-			}
-			if (number % 2 == 0) {
+			while (number % 2 == 0) {
 				result.push_back(2);
 				number /= 2;
 			}
