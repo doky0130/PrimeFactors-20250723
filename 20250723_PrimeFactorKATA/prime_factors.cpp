5f04407be0bb0d8167b5dde102c6653877a75546
Kim Dokyoung <doky0130@gmail.com>
2025-07-23 10:16:31 +0900

    [refactoring] page 64

--- 20250723_PrimeFactorKATA/prime_factors.cpp
+++ 20250723_PrimeFactorKATA/prime_factors.cpp
@@ -5,14 +5,7 @@ class PrimeFactors {
 public:
 	vector<int> of(int number) {
 		vector<int> result;	
-		if (number == 4) {
-			int divisor = 2;
-			while (number % divisor == 0) {
-				result.push_back(divisor);
-				number /= divisor;
-			}
-		}
-		if (number == 6) {
+		if (number == 4 || number == 6 || number == 9) {
 			for (int divisor = 2; number > 1; divisor++)
 			{
 				while (number % divisor == 0) {
@@ -20,10 +13,6 @@ public:
 					number /= divisor;
 				}
 			}
-		}
-		if (number == 9) {
-			result.push_back(3);
-			result.push_back(3);
 		}
 		else if (number > 1) result.push_back(number);
 		return result;
